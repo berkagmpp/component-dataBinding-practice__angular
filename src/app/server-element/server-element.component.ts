56fe18fc4e26ef5cdadbfd12e0ca54aa1659a966
berkagmpp <berkagmpp@gmail.com>
2022-08-26 12:54:40 +1200

    feat: Add a custom property from server-elementCompo to appCompo using @Input() of @angular/core

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,5 +1,4 @@
-import { StringMap } from '@angular/compiler/src/compiler_facade_interface';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
     selector: 'app-server-element',
@@ -7,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent implements OnInit {
-    element: {type: string, name: string, content: string};
+    @Input() element: {type: string, name: string, content: string};
     
     constructor() { }
 
